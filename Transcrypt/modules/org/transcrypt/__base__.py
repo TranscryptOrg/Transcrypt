ad6fdb3eb8332718536d3a9002c5b95cf54ceae7
JdeH <info@qquick.org>
2016-01-18 21:35:59 +0100

    Import as, import from and import from as added

--- Transcrypt/modules/org/transcrypt/__base__.py
+++ Transcrypt/modules/org/transcrypt/__base__.py
@@ -5,7 +5,7 @@
 class __Envir__:
 	def __init__ (self):
 		self.transpilerName = 'transcrypt'
-		self.transpilerVersion = '0.0.27'
+		self.transpilerVersion = '0.0.29'
 		
 __envir__ = __Envir__ ()
 
