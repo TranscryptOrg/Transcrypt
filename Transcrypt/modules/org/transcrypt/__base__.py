9cbd240836cdcf137b02d7bbdb63833654721267
JdeH <info@qquick.org>
2016-01-21 10:15:02 +0100

    version upped

--- Transcrypt/modules/org/transcrypt/__base__.py
+++ Transcrypt/modules/org/transcrypt/__base__.py
@@ -5,7 +5,7 @@
 class __Envir__:
 	def __init__ (self):
 		self.transpilerName = 'transcrypt'
-		self.transpilerVersion = '0.0.40'
+		self.transpilerVersion = '0.0.41'
 		self.targetSubDir = '__javascript__'
 		
 __envir__ = __Envir__ ()
