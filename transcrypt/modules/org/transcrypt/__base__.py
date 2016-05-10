97490e81542c4bb89b6fb6ce8fce3c84ee1c08ef
JdeH <info@qquick.org>
2016-05-10 16:42:27 +0200

    Multi-level sourcemaps and annotated target code, version bumped to match PyPi

--- transcrypt/modules/org/transcrypt/__base__.py
+++ transcrypt/modules/org/transcrypt/__base__.py
@@ -5,7 +5,7 @@
 class __Envir__:
 	def __init__ (self):
 		self.transpiler_name = 'transcrypt'
-		self.transpiler_version = '3.5.144'
+		self.transpiler_version = '3.5.148'
 		self.target_subdir = '__javascript__'
 		
 __envir__ = __Envir__ ()
