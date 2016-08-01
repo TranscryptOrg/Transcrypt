94f5b0c44704d818c945a02200a1f07c317ae8e8
JdeH <info@qquick.org>
2016-08-01 20:58:54 +0200

    Cleanup after committing time module + autotest + shipment test

--- Transcrypt-3.5.196/transcrypt/demos/terminal_demo/terminal_demo.py
+++
@@ -1,9 +0,0 @@
-name = input ('What''s your name? ')
-print ('Hi ', name, 'I am your computer.')
-
-age = float (input ('How old are you? '))
-if age < 18:
-	print ('Sorry ', name, age, 'is to young to drive a car in the Netherlands.')
-else:
-	print ('OK ', name, age, 'is old enough to drive a car in the Netherlands.')
-
--- Transcrypt-3.5.196/transcrypt/demos/turtle_demos/READ_ME_BEFORE_YOU_COMPILE.txt
+++
@@ -1,2 +0,0 @@
-The turtle demos have to be compiled with the switch: -p .user
-This is because the modules are initialized after page load by an explicit call: <modulename> ()
