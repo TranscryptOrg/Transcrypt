740e995b440042a777bd87a2bb78313df4a0d2d5
Carl Allendorph <carl@resonantcircuitlabs.com>
2016-11-22 14:39:56 -0800

    Converted tabs to spaces

--- transcrypt/development/automated_tests/re/basic_jsre.py
+++ transcrypt/development/automated_tests/re/basic_jsre.py
@@ -8,23 +8,23 @@ __pragma__("noskip")
 from basictests import *
 
 def run (test):
-	""" basic tests of the re engine. The point is to
-	exercise most of the methods to make sure they behave as
-	expected. These tests are expected to provide exhaustive
-	coverage of the regex engine.
-	"""
-	checkFlagsExist(test)
-	escapeTests(test)
-	checkMatchProperties(test, re.JSSTRICT)
-	checkRegexProperties(test, re.JSSTRICT)
+    """ basic tests of the re engine. The point is to
+    exercise most of the methods to make sure they behave as
+    expected. These tests are expected to provide exhaustive
+    coverage of the regex engine.
+    """
+    checkFlagsExist(test)
+    escapeTests(test)
+    checkMatchProperties(test, re.JSSTRICT)
+    checkRegexProperties(test, re.JSSTRICT)
 
-	checkIgnoreCase(test, re.JSSTRICT)
+    checkIgnoreCase(test, re.JSSTRICT)
 
-	checkSearchWithGroups(test, re.JSSTRICT)
-	checkMatchOps(test, re.JSSTRICT)
-	checkFullMatchOps(test, re.JSSTRICT)
-	checkFindAllOps(test, re.JSSTRICT)
-	checkSplitOps(test, re.JSSTRICT)
-	checkSubOps(test, re.JSSTRICT)
-	checkSyntaxErrors(test, re.JSSTRICT)
-	checkFindIter(test, re.JSSTRICT)
+    checkSearchWithGroups(test, re.JSSTRICT)
+    checkMatchOps(test, re.JSSTRICT)
+    checkFullMatchOps(test, re.JSSTRICT)
+    checkFindAllOps(test, re.JSSTRICT)
+    checkSplitOps(test, re.JSSTRICT)
+    checkSubOps(test, re.JSSTRICT)
+    checkSyntaxErrors(test, re.JSSTRICT)
+    checkFindIter(test, re.JSSTRICT)
