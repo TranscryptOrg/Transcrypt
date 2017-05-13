280ac50b90ff69736433a4992ff3d0e96f1ab500
JdeH <info@qquick.org>
2017-05-13 17:14:10 +0200

    Minor fixes

--- transcrypt/demos/three_demo/three_demo.py
+++ transcrypt/demos/three_demo/three_demo.py
@@ -13,14 +13,14 @@ cube = api.Mesh (geometry, material)
 scene.add (cube)
 
 ambientLight = api.AmbientLight (0x0000ff, 0.5)
-scene.add (ambientLight );
+scene.add (ambientLight )
 
 directionalLight0 = api.DirectionalLight (0xff0000, 0.5)
 scene.add (directionalLight0);
 
 directionalLight1 = api.DirectionalLight (0x00ff00, 0.5)
 directionalLight1.position.set (50, 50, 50)
-scene.add (directionalLight1);
+scene.add (directionalLight1)
 
 camera.position.z = 5
 
