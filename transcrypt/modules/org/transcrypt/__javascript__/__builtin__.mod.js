ec2c528a4719206171ee49adec50e474b01c02a7
JdeH <info@qquick.org>
2018-01-23 11:16:26 +0100

    callable (None) now returns False

--- transcrypt/modules/org/transcrypt/__javascript__/__builtin__.mod.js
+++ transcrypt/modules/org/transcrypt/__javascript__/__builtin__.mod.js
@@ -375,15 +375,16 @@ __pragma__ ('endif')
         }
     };
     __all__.isinstance = isinstance;
-
+    
     var callable = function (anObject) {
-        if ( typeof anObject == 'object' && '__call__' in anObject ) {
+        if (anObject && typeof anObject == 'object' && '__call__' in anObject) {
             return true;
         }
         else {
             return typeof anObject === 'function';
         }
     };
+    
     __all__.callable = callable;
 
     // Repr function uses __repr__ method, then __str__, then toString
