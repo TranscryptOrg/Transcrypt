edcaea919e51a125b9686c95cbdc519b6c82aad3
JdeH <info@qquick.org>
2018-08-30 20:15:27 +0200

    Version bumped 2

--- transcrypt/modules/org/transcrypt/__envir__.js
+++ transcrypt/modules/org/transcrypt/__envir__.js
@@ -1,5 +1,9 @@
 __envir__.interpreter_name = 'python';
 __envir__.transpiler_name = 'transcrypt';
 __envir__.executor_name = __envir__.transpiler_name;
+<<<<<<< HEAD
 __envir__.transpiler_version = '3.7.4';
+=======
+__envir__.transpiler_version = '3.7.2';
+>>>>>>> 6020c86... Fix for ClassVar bug
  
