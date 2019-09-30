4b8b63e2c3e278d009ac011d77dd285f485a3a26
Andrey Golynskiy <andrey.gtx@gmail.com>
2019-09-30 03:47:45 +0300

    Reimplemented React demo using hooks.

--- transcrypt/demos/react_demo/react_demo.py
+++ transcrypt/demos/react_demo/react_demo.py
@@ -10,24 +10,44 @@ def render(react_element, destination_id, callback=lambda: None):
     ReactDOM.render(react_element, container, callback)
 
 
+useState = React.useState
+useEffect = React.useEffect
+useRef = React.useRef
+
+
+def useInterval(func, delay):
+    ref = useRef(func)
+    ref.current = func
+
+    def setup():
+        id = setInterval(lambda: ref.current(), delay)
+        return lambda: cleanInterval(id)
+
+    useEffect(setup, [delay])
+
+
 # Create a component
 
+def Hello(props):
+    count, setCount = useState(0)
 
-Hello = React.createClass({
-    'displayName': 'Hello',
+    def updateCounter():
+        setCount(count+1)
 
-    'getInitialState': lambda: {'counter': 0},
+    useInterval(updateCounter, 1000)
 
-    'updateCounter': lambda: (this.setState({'counter': this.state['counter']+1})),
-
-    'componentDidMount': lambda: (setInterval(this.updateCounter, 1000)),
-
-    'render': lambda: h('div', {'className': 'maindiv'},
-                          h('h1', None, 'Hello ', this.props['name']),
-                          h('p', None, 'Lorem ipsum dolor sit ame.'),
-                          h('p', None, 'Counter: ', this.state['counter'])
-                        )
-})
+    return h(
+        'div',
+        {'className': 'maindiv'},
+        h('h1', None, 'Hello ', props['name']),
+        h('p', None, 'Lorem ipsum dolor sit ame.'),
+        h('p', None, 'Counter: ', count),
+        h(
+            'button',
+            {'onClick': updateCounter},
+            'Increment',
+        )
+    )
 
 
 # Render the component in a 'container' div
