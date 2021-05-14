fd22610423d5c019ce6f8a0a828ac7aae3ee3f91
JdeH <info@geatec.com>
2021-05-14 18:26:52 +0200

    feature_py39 merged into master

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read (*paths):
 
 setup (
 	name = 'Transcrypt',
-	version = '3.8.0',
+	version = '3.9.0',
 	description = 'Python to JavaScript transpiler, supporting multiple inheritance and generating lean, highly readable code',
 	long_description = (
 		read ('README.rst')
